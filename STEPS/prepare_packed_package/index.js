83c5a16b064410ba7f1c4189a78b6fa8d7f0769b
ofer shaham <ofer.shaham@gmail.com>
2021-08-16 11:55:46 +0300

    duplicate indicator script

--- STEPS/prepare_packed_package/index.js
+++ STEPS/prepare_packed_package/index.js
@@ -1,21 +1,14 @@
-// const db = new better_sqlite3(); //.Database(":memory:");
-// db.Database().execute("CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)");
-// db.Database().execute("INSERT INTO test VALUES (1, 'Hello')");
-// db.Database().execute("INSERT INTO test VALUES (2, 'World')");
-// db.Database().execute("INSERT INTO test VALUES (3, 'Goodbye')");
-
-// db.execute("SELECT * FROM test");
-
 const Database = require("better-sqlite3");
 const db = new Database("analytics1.sqlite");
 
 var runSafe = function (table, alter) {
   try {
     output = db.prepare(alter).run();
-    console.log(output);
+    // console.log(output);
+    console.log("dbstat is ENABLED");
   } catch (e) {
     console.error(e);
-    // Table does not need altering
+    console.log("dbstat is DISABLED");
   }
 };
 
